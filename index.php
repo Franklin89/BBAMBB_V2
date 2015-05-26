a4707bcc93bf301b479612a9fa535c53a867600c
Matteo Locher <matteo.locher@ml-software.ch>
2015-05-26 15:07:11 +0200

    Page loader...History...footer

--- index.php
+++ index.php
@@ -63,6 +63,10 @@ include_once 'languages/'.$lang_file;
 
 <body>
 
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+
     <ul id="language">
         <li><a href="?lang=de">DE</a></li>
         <li><a href="?lang=fr">FR</a></li>
@@ -71,28 +75,30 @@ include_once 'languages/'.$lang_file;
 
     <div id="error"></div>
 
-    <div class="main_container">
+    <div class="main_container" style="padding-top: 100px">
 
-        <div class="container">
-            <h1><?php echo $lang['welcome']; ?></h1>
+        <div class="container" style="margin-bottom: 70px">
+            <a href="">
+                <img src="data/images/small_logo.png" height="150px">
+            </a>
         </div>
 
         <!--Nav Bar-->
         <div id="navbar" class="container">
             <div id="bbambb">
-                <a href="bbambb.php" class="navigation"><?php echo $lang['bbambb'] ?></a>
+                <a href="#bbambb" class="navigation"><?php echo $lang['bbambb'] ?></a>
             </div>
             <div id="rooms">
-                <a href="rooms.php" class="navigation"><?php echo $lang['rooms'] ?></a>
+                <a href="#rooms" class="navigation"><?php echo $lang['rooms'] ?></a>
             </div>
             <div id="locality">
-                <a href="locality.php" class="navigation"><?php echo $lang['locality'] ?></a>
+                <a href="#locality" class="navigation"><?php echo $lang['locality'] ?></a>
             </div>
             <div id="offers">
-                <a href="offers.php" class="navigation"><?php echo $lang['offers'] ?></a>
+                <a href="#offers" class="navigation"><?php echo $lang['offers'] ?></a>
             </div>
             <div id="reservation">
-                <a href="reservation.php" class="navigation"><?php echo $lang['reservation'] ?></a>
+                <a href="#reservation" class="navigation"><?php echo $lang['reservation'] ?></a>
             </div>
         </div>
 
@@ -100,10 +106,36 @@ include_once 'languages/'.$lang_file;
         <div id="content" class="container">
             <h1><?php echo $lang['welcome']; ?></h1>
         </div>
+
+    </div>
+
+    <!--Footer-->
+    <div id="footer_main_container">
+        <div id="footer_container">
+            <table class="footer_table_setup">
+                <tr>
+                    <td>BED AND BREAKFAST AM BÜHLBERG</td>
+                    <td>|</td>
+                    <td>Bühlbergstrasse 108</td>
+                    <td>|</td>
+                    <td>3775 Lenk im Simmental</td>
+                    <td>|</td>
+                    <td>+41 (0)33 733 04 19</td>
+                    <td>|</td>
+                    <td><a href="mailto:info@bbambb.ch">info@bbambb.ch</a></td>
+                    <td id="space"></td>
+                    <!--<td><a href="http://www.ml-software.ch">Copyright &copy; 2013 ML-Software</a></td>-->
+                    <td><a href="files/Impressum.pdf" target="_blank">IMPRESSUM</a></td>
+                    <td>|</td>
+                    <td><a href="files/AGB.pdf" target="_blank">AGB</a></td>
+                </tr>
+            </table>
+        </div>
     </div>
 
     <!--Scripts-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="application/javascript" src="lib/jquery.bbq.1.2.1.js"></script>
     <script type="application/javascript" src="js/app.js"></script>
 
 </body>
--- js/app.js
+++ js/app.js
@@ -2,33 +2,58 @@
  * Created by mlocher on 5/19/2015.
  */
 (function() {
-    function LoadContent(page)
-    {
-        //Load the content
-        $('#content').load(page, function( response, status, xhr ) {
-            if ( status == "error" ) {
-                var msg = "Sorry but there was an error: ";
+    $(document).ready(function () {
+        // Register Navigation event
+        // Bind an event to window.onhashchange that, when the history state changes,
+        // gets the url from the hash and displays either our cached content or fetches
+        // new content to be displayed.
+        $(window).bind('hashchange', function (e) {
+
+            // Get the hash (fragment) as a string
+            var pageName = $.param.fragment();
+
+            if(pageName && pageName != "") {
+                setActiveLink(pageName);
+                LoadContent(pageName);
+            }
+        });
+
+        //Remove page loader
+        setTimeout(function(){
+            $('#status').fadeOut(); // will first fade out the loading animation
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website
+            $(window).trigger('hashchange');
+        }, 750);
+
+
+    });
+
+    function LoadContent(page){
+        $('#content').slideUp( 1500, function(){
+            //Load the content
+            $('#content').load(page + ".php", function( response, status, xhr ) {
                 var content = $('#content');
-                content.html('<h1>' + msg + xhr.status + " " + xhr.statusText + '</h1>');
-                content.addClass('error');
-            }
-            else {
-                $('#content').removeClass('error');
-            }
+
+                if ( status == "error" ) {
+                    var msg = "Sorry but there was an error: ";
+                    content.html('<h1>' + msg + xhr.status + " " + xhr.statusText + '</h1>');
+                    content.addClass('error');
+                }
+                else {
+                    content.removeClass('error');
+                    content.slideDown(1500);
+                }
+            });
         });
     }
 
-    $(document).ready(function () {
-        $('#test').on('click', function (e) {
-            alert('test was clicked');
-        });
+    function setActiveLink(page) {
 
-        //Navigation
-        $('.navigation').on('click', function(e){
-            //Retrieve menu name that was clicked
-            var page = $(this).attr('href');
-            LoadContent(page);
-            return false; //return false so that the link is not executed
-        });
-    });
+        //Remove current active link
+        $("#navbar .active").removeClass("active");
+
+        //Set active link
+        $("#" + page).addClass("active");
+    }
+
 })();
