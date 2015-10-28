0470b749510525fc5f251525899c0112de9a4f34
Matteo Locher <matteo.locher@geowerkstatt.ch>
2015-10-28 14:21:17 +0100

    Init main grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -6,47 +6,91 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-
         jekyll: {
-            server : {
-                options : {
-                    src: 'src',
-                    config: 'src/_config.yml',
-                    dest: 'build/dev',
-                    serve : true,
-                    auto : true
-                }
+          dev: {
+              options : {
+                  src: 'src',
+                  config: 'src/_config.yml',
+                  dest: 'build/dev'
+              }
+          },
+          prod: {
+              options : {
+                  src: 'src',
+                  config: 'src/_config.yml',
+                  dest: 'build/prod'
+              }
+          }
+        },
+        htmlhint:{
+          dev:{
+              src:['build/dev/**/*.html']
+          },
+          prod:{
+            src:['build/prod/**/*.html']
+          }
+        },
+        bowercopy:{
+          options: {
+            // Bower components folder will be removed afterwards
+            clean: false
+          },
+          // Javascript
+          js_dev: {
+            options: {
+                destPrefix: 'build/dev/js'
             },
-            dev: {
-                options : {
-                    src: 'src',
-                    config: 'src/_config.yml',
-                    dest: 'build/dev'
-                }
-            },
-            prod: {
-                options : {
-                    src: 'src',
-                    config: 'src/_config.yml',
-                    dest: 'build/prod'
-                }
+            files: {
+                'jquery.js': 'jquery/dist/jquery.js',
+                'smooth-scroll.js': 'smooth-scroll/dist/js/smooth-scroll.js',
+                'jquery.sticky-kit.js': 'sticky-kit/jquery.sticky-kit.js'
             }
+          },
+          css_dev: {
+            options: {
+                destPrefix: 'build/dev/css'
+            },
+            files: {
+                'bootstrap.css': 'bootstrap/dist/css/bootstrap.css'
+            }
+          },
+          js_prod: {
+            options: {
+                destPrefix: 'build/prod/js'
+            },
+            files: {
+                'jquery.js': 'jquery/dist/jquery.js',
+                'smooth-scroll.js': 'smooth-scroll/dist/js/smooth-scroll.js',
+                'jquery.sticky-kit.js': 'sticky-kit/jquery.sticky-kit.js'
+            }
+          },
+          css_prod: {
+            options: {
+                destPrefix: 'build/prod/css'
+            },
+            files: {
+                'bootstrap.css': 'bootstrap/dist/css/bootstrap.css'
+            }
+          }
         },
         watch: { // for development run 'grunt watch'
             jekyll: {
                 files: ['src/**/**.*'],
-                tasks: ['jekyll:dev']
+                tasks: ['ci']
             }
         }
     });
 
     // taks
-    grunt.registerTask('default', 'jekyll:dev');
-    grunt.registerTask('serve', 'jekyll:server');
-    grunt.registerTask('watch', ['jekyll:dev', 'watch']);
+    grunt.registerTask('ci', ['jekyll:dev', 'htmlhint:dev', 'bowercopy:js_dev', 'bowercopy:css_dev']) // Continous Integration Build
+    grunt.registerTask('release', ['jekyll:prod', 'htmlhint:prod', 'bowercopy:js_prod', 'bowercopy:css_prod']) // Production Build
+
+    grunt.registerTask('default', ['ci']);
+    grunt.registerTask('watch', ['ci', 'watch']);
 
     // plugin tasks
     grunt.loadNpmTasks('grunt-jekyll');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-htmlhint');
 };
